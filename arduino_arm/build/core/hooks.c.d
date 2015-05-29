a0e52140a180e6c824ee0d900dd6fd088cbe675a
midmva <midv@tut.by>
2015-05-29 18:20:11 +0300

    delete build

--- arduino_arm/build/core/hooks.c.d
+++
@@ -1,2 +0,0 @@
-/opt/Qt5.4.1/Tools/QtCreator/share/qtcreator/templates/wizards/qt-creator-arduino-arm/arduino_arm/build/core/hooks.c.o: \
- /home/midv/Arduino/hardware//arduino/sam/cores/arduino/hooks.c
--- arduino_arm/build/core/iar_calls_sam3.c.d
+++
@@ -1,2 +0,0 @@
-/opt/Qt5.4.1/Tools/QtCreator/share/qtcreator/templates/wizards/qt-creator-arduino-arm/arduino_arm/build/core/iar_calls_sam3.c.o: \
- /home/midv/Arduino/hardware//arduino/sam/cores/arduino/iar_calls_sam3.c
