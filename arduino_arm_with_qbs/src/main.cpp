700a1d9c6c143d69c97be4299c364616716d0cac
midmva <midv@tut.by>
2015-06-10 12:10:27 +0300

    add qbs

---
+++ arduino_arm_with_qbs/src/main.cpp
@@ -0,0 +1,79 @@
+/*
+  main.cpp - Main loop for Arduino sketches
+  Copyright (c) 2005-2013 Arduino Team.  All right reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#define ARDUINO_MAIN
+#include "Arduino.h"
+
+/*
+extern void SysTick_Handler( void )
+{
+  // Increment tick count each ms
+  TimeTick_Increment() ;
+}
+*/
+
+// Weak empty variant initialization function.
+// May be redefined by variant files.
+void initVariant() __attribute__((weak));
+void initVariant() { }
+
+/*
+ * \brief Main entry point of Arduino application
+ */
+int main( void )
+{
+    init();
+
+    initVariant();
+
+    delay(1);
+
+#if defined(USBCON)
+    USBDevice.attach();
+#endif
+
+    setup();
+
+    for (;;)
+    {
+        loop();
+        if (serialEventRun) serialEventRun();
+    }
+
+    return 0;
+}
+
+void setup() {
+  // initialize digital pin 13 as an output.
+  pinMode(13, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(50);              // wait for a second
+  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);              // wait for a second
+}
+
+extern "C"
+void __cxa_pure_virtual() {
+    while (true);
+}
+
